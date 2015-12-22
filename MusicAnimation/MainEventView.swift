785f98e8943be36d76a30d1a9f897e71a157a33b
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2015-12-22 17:39:12 -0500

    animation with one song

---
+++ MusicAnimation/MainEventView.swift
@@ -0,0 +1,13 @@
+//
+//  MainEventView.swift
+//  MusicAnimation
+//
+//  Created by Andrew Burns on 12/16/15.
+//  Copyright © 2015 Andrew Burns. All rights reserved.
+//
+
+import Cocoa
+
+class MainEventView: UIView {
+
+}
